44165f217a111ac2f612d06e2a929dcf225e43db
GoldenMine0502 <ehe123@naver.com>
2023-03-05 18:52:13 +0900

    클라이언트만 다운로드 기능 추가 (테스트 X)

---
+++ src/main/kotlin/kr/goldenmine/inuminecraftlauncher/launcher/MinecraftDataDownloader.kt
@@ -0,0 +1,74 @@
+package kr.goldenmine.inuminecraftlauncher.launcher
+
+import kr.goldenmine.inuminecraftlauncher.LauncherSettings
+import kr.goldenmine.inuminecraftlauncher.download.ServerRequest
+import kr.goldenmine.inuminecraftlauncher.util.Compress
+import kr.goldenmine.inuminecraftlauncher.util.OS_NAME
+import kr.goldenmine.inuminecraftlauncher.util.writeResponseBodyToDisk
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.io.File
+
+class MinecraftDataDownloader(
+    private val launcherSettings: LauncherSettings
+) {
+    private val log: Logger = LoggerFactory.getLogger(MinecraftDataDownloader::class.java)
+
+    fun downloadJava() {
+        log.info("downloading java...")
+
+        val javaFileName = launcherSettings.instanceSettings.javaVersionSpecific[OS_NAME]
+            ?: throw RuntimeException("not supported OS")
+        val javaFileNameWithExtension = "$javaFileName.zip"
+        val javaRoute = File(launcherSettings.launcherDirectories.javaDirectory, "$OS_NAME/$javaFileName")
+        val javaRouteWithExtension =
+            File(launcherSettings.launcherDirectories.javaDirectory, "$OS_NAME/$javaFileNameWithExtension")
+
+        // 존재하지 않을 때만 다운로드
+        if (!javaRouteWithExtension.exists()) {
+            val response = ServerRequest.SERVICE.downloadJava(javaFileNameWithExtension).execute()
+            if (!response.isSuccessful) throw RuntimeException("failed to download java. response is not successful.")
+
+            val body = response.body() ?: throw RuntimeException("failed to download java. no body.")
+
+            log.info("saving java to $javaRoute")
+            writeResponseBodyToDisk(javaRouteWithExtension, body)
+
+            log.info("unzipping java...")
+            Compress().unZip(javaRouteWithExtension.absolutePath, javaRoute.absolutePath)
+        } else {
+            log.info("java already exists. $javaFileName")
+        }
+    }
+
+    fun downloadMods() {
+        log.info("downloading mods...")
+
+        val modsFolder = File(
+            launcherSettings.launcherDirectories.instancesDirectory,
+            "${launcherSettings.instanceSettings.instanceName}/mods"
+        )
+        modsFolder.mkdirs()
+
+        launcherSettings.instanceSettings.mods.forEach { modName ->
+            val modFile = File(modsFolder, modName)
+
+            // 존재하지 않을 때만 다운로드
+            if (!modFile.exists()) {
+                val response = ServerRequest.SERVICE.downloadMod(modName).execute()
+                if (!response.isSuccessful) throw RuntimeException("failed to download mod $modName. response is not successful.")
+
+                val body = response.body() ?: throw RuntimeException("failed to download mod $modName. no body.")
+
+                writeResponseBodyToDisk(modFile, body)
+            } else {
+                log.info("mod $modName already exists.")
+            }
+        }
+    }
+
+    fun download() {
+        downloadJava()
+        downloadMods()
+    }
+}
